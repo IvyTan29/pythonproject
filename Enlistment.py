f6598c73f06e8b4b458b367775087092da275f21
Ivy Tan <tanivy31@gmail.com>
2019-12-04 11:53:20 +0800

    Initial Commit

---
+++ Enlistment.py
@@ -0,0 +1,11 @@
+class Enlistment:
+    classes = []
+    user = ""
+
+    def logIn (self, idnumber, password):
+        if (self.user.getIDNumber != idnumber):
+            print ("")
+            return False
+    
+    def setUser (self, user):
+        self.user = user
