29813724b5723f0e268b1cad44081433b4dc7b9d
Ivy Tan <tanivy31@gmail.com>
2019-12-04 12:39:59 +0800

    Change user to a list

--- Enlistment.py
+++ Enlistment.py
@@ -1,11 +1,25 @@
 class Enlistment:
     classes = []
-    user = ""
+    user = []
 
     def logIn (self, idnumber, password):
-        if (self.user.getIDNumber != idnumber):
-            print ("")
+        isFound = False
+        index = ""
+        i = 0
+        for i in range(len(user)):
+            if (self.user[i].getIDNumber() == idnumber):
+                isFound = True
+                index = i
+        
+        if (isFound == False):
+            print ("User not found.")
             return False
-    
+        else:
+            if (self.user[index].getPassword() != password):
+                print ("Password Wrong")
+                return False
+            else:
+                return True
+        
     def setUser (self, user):
         self.user = user
