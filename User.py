4965e1009bd2f5f49524b483a3a89a1ec1b78a32
Ivy Tan <tanivy31@gmail.com>
2019-12-04 11:51:50 +0800

    Initial commit

---
+++ User.py
@@ -0,0 +1,14 @@
+class User:
+    idnumber = ""
+    password = ""
+
+    def __init__(self,idnumber,password):
+        self.idnumber = idnumber
+        self.password = password
+
+    
+    def getIDNumber(self) :
+        return self.idnumber
+
+    def getPassword(self) :
+        return self.password
